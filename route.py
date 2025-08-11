d9c9055a299153f86230b38493b038d01ad60799
VoidXTora <priyanshuprasad853@gmail.com>
2025-08-11 09:02:43 +0530

    Update route.py

--- route.py
+++ route.py
@@ -1,20 +1,20 @@
-from aiohttp import web
-import asyncio
-from config import WEBHOOK_PATH  # Import directly
+# from aiohttp import web
+# import asyncio
+# from config import WEBHOOK_PATH  # Import directly
 
-async def handle_webhook(request):
-    try:
-        data = await request.json()
-        headers = {k: v for k, v in request.headers.items()}
-        asyncio.create_task(request.app["bot"].process_webhook_update(data, headers))
-        return web.Response(text="OK")
-    except Exception as e:
-        print(f"[ERROR] Webhook update failed: {e}")
-        return web.Response(status=500, text="Error")
+#async def handle_webhook(request):
+   # try:
+ #       data = await request.json()
+#        headers = {k: v for k, v in #request.headers.items()}
+        #asyncio.create_task(request.app["bot"].process_web#hook_update(data, headers))
+#        return web.Response(text="OK")
+#    except Exception as e:
+#        print(f"[ERROR] Webhook update failed: #{e}")
+#        return web.Response(status=500, #text="Error")
 
-async def web_server(bot):
-    app = web.Application()
-    app["bot"] = bot
-    app.router.add_post(f"/{WEBHOOK_PATH}", handle_webhook)
-    app.router.add_get("/", lambda req: web.Response(text="Bot is running!"))
-    return app
+#async def web_server(bot):
+ #   app = web.Application()
+#    app["bot"] = bot
+#    app.router.add_post(f"/{WEBHOOK_PATH}", #handle_webhook)
+#    app.router.add_get("/", lambda req: #web.Response(text="Bot is running!"))
+#    return app
